180dc308e1e17537e7243662315aff0fea2dac85
Raí Rodrigues <142638875+Rairodrigues040@users.noreply.github.com>
2025-12-06 16:52:15 -0300

    desafio DIO resolvido
    Added user registration and formation level to the education content model.

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,38 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario (val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+       inscritos.add(usuario)
+       println("O usuário ${usuario.nome} na formação $nome")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val kotlinEstrutural =  ConteudoEducacional("Paradgma Estrutural", 90)
+    val oo = ConteudoEducacional("Paradgma Orientado a Objetos", 90)
+    
+    val formacaoKt = Formacao(
+        "Formação Kotlin Mobile",
+        conteudos = listOf(kotlinEstrutural, oo),
+        Nivel.DIFICIL
+    )
+    
+    val usuario1 = Usuario("Raí")
+    val usuario2 = Usuario("Luana")
+    val usuario3 = Usuario("Lula")
+    
+    formacaoKt.matricular(usuario1)
+    formacaoKt.matricular(usuario2)
+    formacaoKt.matricular(usuario3)
+    
+    println("Inscritos na formação:")
+    formacaoKt.inscritos.forEach { println(it.nome) }
 }
